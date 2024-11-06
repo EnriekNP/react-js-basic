e354e0843c6cfec0e48fe6f141f973b76d81fe25
Enriek Naldo Patera <Pateraenriek@gmail.com>
2024-11-06 18:59:12 +0700

    feat: add jsx rules

---
+++ 01.Fundamentals/02.JSX/JSXRules.jsx
@@ -0,0 +1,12 @@
+const JSXRules = () => {
+    return (
+        <div>
+            <h1>JSX Rules</h1>
+            <p>JSX must return a <b>single parent element</b></p>
+            <p>JSX element must be<b>properly closed</b></p>
+            <p>JSX attribute are written using <b>camelCase</b> (eg., className instead of class)</p>
+        </div>
+    )
+}
+
+export default JSXRules
