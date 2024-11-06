7dc9675b99de74a24503b2f8c15696e61569eb32
Enriek Naldo Patera <Pateraenriek@gmail.com>
2024-11-06 19:14:56 +0700

    feat: add multiple component notes & exercise

---
+++ 01.Fundamentals/03.Multiple Component/MainContent.jsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+    return (
+        <main>
+            <h2>Main Content</h2>
+            <p>HEllo world i am coming to become the greatest software engineer ever</p>
+        </main>
+    )
+}
+
+export default MainContent
